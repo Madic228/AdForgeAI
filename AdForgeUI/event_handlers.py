aeaba170301b86514c164a97404af0c8297384a5
Rzayev Metin <rzayevmetin@mail.ru>
2024-05-22 12:31:19 +0500

    исправление отображения картинок. слайдер 0.7. эл комбобокс. правая часть окна. кнопка очистить все

--- AdForgeUI/event_handlers.py
+++ AdForgeUI/event_handlers.py
@@ -1,4 +1,5 @@
-from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QPoint
+from PyQt5.QtGui import *
 
 def toggle_icon_and_move(window):
     if window.is_down:
@@ -7,33 +8,39 @@ def toggle_icon_and_move(window):
         window.droparrow_button.setIcon(QIcon('images/up-arrow.png'))
     window.is_down = not window.is_down
 
-    # Можно добавить логику для перемещения кнопки и диалогового окна
+    def toggle_icon_and_move(self):
+        # Переключение иконки
+        if self.is_down:
+            self.droparrow_button.setIcon(QIcon('images/down-arrow.png'))
+        else:
+            self.droparrow_button.setIcon(QIcon('images/up-arrow.png'))
+        self.is_down = not self.is_down  # переключаем состояние
 
-    # def toggle_icon_and_move(self):
-    #     # Переключение иконки
-    #     if self.is_down:
-    #         self.droparrow_button.setIcon(QIcon('images/down-arrow.png'))
-    #     else:
-    #         self.droparrow_button.setIcon(QIcon('images/up-arrow.png'))
-    #     self.is_down = not self.is_down  # переключаем состояние
+        # Перемещение кнопки и диалогового окна
+        current_pos_button = self.droparrow_button.pos()
+        current_pos_dialog = self.input_dialog.pos() if self.input_dialog else QPoint(0, 0)
+        current_pos_line = self.line_4.pos() if self.line_4 else QPoint(0, 0)
 
-        # # Перемещение кнопки и диалогового окна
-        # current_pos_button = self.droparrow_button.pos()
-        # current_pos_dialog = self.input_dialog.pos() if self.input_dialog else QPoint(0, 0)
-        # current_pos_line = self.line_4.pos() if self.line_4 else QPoint(0, 0)
-        #
-        # # if current_pos_button == QPoint(230, 490) and current_pos_dialog == QPoint(210, 580) and current_pos_line == QPoint(0, 487):
-        # #     self.droparrow_button.move(230, 150)
-        # #     if self.input_dialog:
-        # #         self.input_dialog.move(210, 240)
-        # #         self.line_4.move(0, 150)
-        # # else:
-        # #     self.droparrow_button.move(230, 490)
-        # #     if self.input_dialog:
-        # #         self.input_dialog.move(210, 580)
-        # #         self.line_4.move(0, 487)
+        if current_pos_button == QPoint(230, 490) and current_pos_dialog == QPoint(210, 580) and current_pos_line == QPoint(0, 487):
+            self.droparrow_button.move(230, 150)
+            if self.input_dialog:
+                self.input_dialog.move(210, 240)
+                self.line_4.move(0, 150)
+        else:
+            self.droparrow_button.move(230, 490)
+            if self.input_dialog:
+                self.input_dialog.move(210, 580)
+                self.line_4.move(0, 487)
 
 
 def slide_it(window, value):
     decimal_value = value / 10.0
     window.tempResTxt.setText(f"{decimal_value:.1f}")
+
+def clear_fields(self):
+    # Очистить все QLineEdit
+    self.headlineEdit.clear()
+    self.audienceEdit.clear()
+    self.keyEdit.clear()
+    self.actionEdit.clear()
+    self.lengthEdit.clear()
--- AdForgeUI/main_window.py
+++ AdForgeUI/main_window.py
@@ -1,8 +1,8 @@
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QMainWindow, QComboBox, QPushButton, QSlider, QLabel, QWidget, QFrame
+from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
 from ui_loader import load_ui
-from event_handlers import toggle_icon_and_move, slide_it
+from event_handlers import toggle_icon_and_move, slide_it, clear_fields
 
 class MyWindow(QMainWindow):
     def __init__(self):
@@ -14,8 +14,6 @@ class MyWindow(QMainWindow):
         self.form = load_ui(self)
         self.setWindowFlags(Qt.FramelessWindowHint)
 
-        # Найдите QComboBox по его имени
-        self.comboBox = self.findChild(QComboBox, 'styleBox')
 
         # Найти кнопку и установить начальную иконку
         self.droparrow_button = self.findChild(QPushButton, 'droparrowBtn')
@@ -31,11 +29,30 @@ class MyWindow(QMainWindow):
         # Настройка QSlider
         self.slider.setRange(0, 10)
         self.slider.setSingleStep(1)
+        self.slider.setValue(7)
+
+        # Обновляем QLabel с текущим значением слайдера в виде десятичного числа
+        initial_value = self.slider.value() / 10.0
+        self.tempResTxt.setText(f"{initial_value:.1f}")
         self.slider.valueChanged.connect(lambda value: slide_it(self, value))
 
         # Найти виджет InputDialog
         self.input_dialog = self.findChild(QWidget, 'InputDialog')
         self.line_4 = self.findChild(QFrame, 'line_4')
+
+        #Найти кнопки генерации текста и очищение полей
+        self.genBtn = self.findChild(QPushButton, 'generationBtn')
+        self.clearBtn = self.findChild(QPushButton, 'clearBtn')
+
+        #Найти поля ввода данных
+        self.headlineEdit = self.findChild(QLineEdit, 'headlineEdit')
+        self.audienceEdit = self.findChild(QLineEdit, 'audienceEdit')
+        self.keyEdit = self.findChild(QLineEdit, 'keyEdit')
+        self.actionEdit = self.findChild(QLineEdit, 'actionEdit')
+        self.lengthEdit = self.findChild(QLineEdit, 'lengthEdit')
+
+        # Подключить кнопку очистки к слоту
+        self.clearBtn.clicked.connect(lambda: clear_fields(self))
 
         # Настройка кнопок управления окном
         self.setup_button('close', self.close_window)
